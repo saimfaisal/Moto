74fedeef0b27a6a25c7294b92e1d85f99e9711ca
saim <saimfaisal.official.com@gmail.com>
2025-06-27 00:50:35 -0700

    size adjust

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -51,8 +51,11 @@ function Navbar({theme,setTheme,onLoginClick,onSignupClick}) {
                     } 
                     
                 </div>
-                <button onClick={onLoginClick} className='btn  text-primary  px-5 py-2 rounded-md border-primary border-2 dark:bg-neutral-800 hover:bg-primary hover:text-white duration-300 dark:hover:bg-primary'>login</button>
-                <button onClick={onSignupClick}className='btn  text-primary  px-5 py-2 rounded-md border-primary border-2 dark:bg-neutral-800 hover:bg-primary hover:text-white duration-300 dark:hover:bg-primary' >Signup</button>
+                <div className='flex gap-3'>
+                    <button onClick={onLoginClick} className='btn  text-primary  px-3 py-1 rounded-md border-primary border-2 dark:bg-neutral-800 hover:bg-primary hover:text-white duration-300 dark:hover:bg-primary'>login</button>
+                <button onClick={onSignupClick}className='btn  text-primary  px-3 py-2 rounded-md border-primary border-2 dark:bg-neutral-800 hover:bg-primary hover:text-white duration-300 dark:hover:bg-primary' >Signup</button>
+                </div>
+              
             </div>
             
         </div>
